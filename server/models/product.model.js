eba1d792b91e71b56450048be01a280ae76bd2c9
Deepika2024 <deepikarao808@gmail.com>
2024-12-10 16:50:22 +0530

    Initial commit

---
+++ server/models/product.model.js
@@ -0,0 +1,135 @@
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        adminRef: {
+            type: Schema.Types.ObjectId,
+            ref: 'Admin',
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        shortDescription: {
+            type: String,
+        },
+        longDescription: {
+            type: String,
+        },
+        quantity: {
+            type: String,
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: 'Category',
+        },
+        subCategory: {
+            type: Schema.Types.ObjectId,
+            ref: 'SubCategory',
+        },
+        brand: {
+            type: Schema.Types.ObjectId,
+            ref: 'Brand',
+        },
+        brandImage: {
+            type: String,
+        },
+        weight: {
+            type: Number,
+        },
+        length: {
+            type: Number,
+        },
+        width: {
+            type: Number,
+        },
+        height: {
+            type: Number,
+        },
+        manufacturingDate: {
+            type: Date,
+        },
+        expiryDate: {
+            type: Date,
+        },
+        specialFeature: {
+            type: String,
+        },
+        mrp: {
+            type: Number,
+        },
+        sellingPrice: {
+            type: Number,
+            required: true,
+        },
+        tax: {
+            type: Number,
+            required: true,
+        },
+        image1: {
+            type: String,
+        },
+        image2: {
+            type: String,
+        },
+        image3: {
+            type: String,
+        },
+        image4: {
+            type: String,
+        },
+        image5: {
+            type: String,
+        },
+        warranty: {
+            type: String,
+        },
+        colour: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Colour',
+            },
+        ],
+        finishType: {
+            type: String,
+        },
+        status: {
+            type: String,
+            enum: ['A', 'D'],
+            default: 'A',
+        },
+        about: [
+            {
+                type: String,
+            },
+        ],
+        isTopDeal: {
+            type: Boolean,
+            default: false,
+        },
+        isExpress: {
+            type: Boolean,
+            default: false,
+        },
+        searchTags: [
+            {
+                type: String,
+            },
+        ],
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('product', productSchema);
